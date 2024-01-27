6ed7ecb21c1f0d815f32c6d6cd39a42eef510da1
Alejo-Vignale <138889322+Alejo-Vignale@users.noreply.github.com>
2024-01-27 20:55:26 -0300

    change

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ window.addEventListener("mousemove", function (e) {
   cursor.style.left = e.clientX - 250 + "px";
   cursor.style.top = e.clientY - 250 + "px";
 });
+
+
+document.querySelector('.aboutMe').addEventListener('click', function(){
+  document.querySelector('.show').style.display =
+          document.querySelector('.show').style.display === 'none'
+            ? 'block'
+            : 'none';
+});
+
+document.querySelector('.myProjects').addEventListener('click', function(){
+  let showSection = document.querySelector('.show');
+  showSection.style.display = showSection.style.display === 'none' ? 'block' : 'none';
+});
